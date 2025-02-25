b82715b69e42fe7e720086108d88b85aff3fb1e1
EdwarVillamizar <villamizar.edwar@gmail.com>
2025-02-25 16:45:51 -0500

    feat: Lab Testing First Complete.

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -3,11 +3,13 @@ package example.cashcard;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class CashcardApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void myFirstTest(){
+		assertThat(1).isEqualTo(42);
 	}
-
 }
